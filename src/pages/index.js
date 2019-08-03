ac61d077c994f816ad922c281a81ab0a35b2c85a
Kyrell Dixon <kyrell.brown@gmail.com>
2019-08-03 15:59:59 -0500

    Add base styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
-import FloatingContainer from "../components/floating-container";
+import FloatingContainer from "../components/floating-container"
+
+import "../global-styles.css"
 
 export default () => (
   <Layout>
